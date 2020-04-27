6b60050c06ae3017d54722c6902f2328c49bd2ea
Francesco Minnocci <ad17fmin@uwcad.it>
2020-04-28 00:28:26 +0200

    Added various dots; more xscreensaver prefs; texclear scrip adjustments; beautiful zathura base16_sol_dark colors and working synctex backward search; new powerlevel10k cfg with customized lean theme:)

---
+++ programs_drivers/robaUtile/SwapCommands.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=4194304
+
+sudo chmod 600 /swapfile
+
+sudo mkswap /swapfile
+
+sudo swapon /swapfile
+
+
+
+/swapfile swap swap defaults 0 0
+
+
+
+
+sudo swapon --show
+
+sudo free -h
+
+
+cat /proc/sys/vm/swappiness
+
+#Append vm.swappiness=60 to /etc/sysctl.conf
