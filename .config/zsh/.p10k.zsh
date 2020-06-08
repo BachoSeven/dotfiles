0c468fdaf67d79593b730849ae7724cb3055c092
Francesco Minnocci <ad17fmin@uwcad.it>
2020-06-08 15:56:12 +0200

    Update p10k colors
    Former-commit-id: 79069b8bde4941520363d59e1cdffd640495a774
    Former-commit-id: 685b96727f610b3a51b8dc1fe54c8c4cc972048f

--- .config/zsh/.p10k.zsh
+++ .config/zsh/.p10k.zsh
@@ -807,7 +807,7 @@
   # Context color in SSH without privileges.
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=7
   # Default context color (no privileges, no SSH).
-  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=7
+  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=5
 
   # Context format when running with privileges: bold user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%B%n@%m'
