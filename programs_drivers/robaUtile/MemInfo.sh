f7d4ca873116a3c64520ab42ad50a6ebad9e2f45
Francesco Minnocci <ad17fmin@uwcad.it>
2020-06-09 15:57:59 +0200

    Cleaning
    Former-commit-id: ee769224b058c694ed06f3230cb60d3b133026b5
    Former-commit-id: 4e4025d594301cd27cce0b896371f0d5806adc8c

--- programs_drivers/robaUtile/MemInfo.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-free -h|awk '/^Mem:/ {print $3 "/" $2}'
-ps axch -o cmd:18,%mem,pid --sort -%mem|head
-# top: zxcVm1t0(w)
--- programs_drivers/robaUtile/printa4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo Inserisci il nome del file da stampare senza estenzione
-read -r a
-cp $a.pdf Temporaneo.pdf
-scp Temporaneo.pdf minnocci@lab1.cs.dm.unipi.it:~/
-ssh -XY minnocci@lab1.cs.dm.unipi.it okular Temporaneo.pdf --print
-ssh minnocci@lab1.cs.dm.unipi.it rm Temporaneo.pdf
-rm Temporaneo.pdf
-echo "File temporanei eliminati "
