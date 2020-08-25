ce56db41a6a677b2f860fa81c13878e3a10b8c79
Francesco Minnocci <ad17fmin@uwcad.it>
2020-08-25 21:48:20 +0200

    Adjust redshift notification hook
    Former-commit-id: adcb89d9569198329175fe4d4b46aec27f88c79e
    Former-commit-id: 916338c181b2653615a34db4f1e25890d399edbe

--- .config/redshift/hooks/notify.sh
+++ .config/redshift/hooks/notify.sh
@@ -2,5 +2,9 @@
 
 case $1 in
 	period-changed)
-		exec notify-send "Redshift" "Goodbye $2, it is now $3."
+		if [ "$3" = "night" ]; then
+			exec notify-send "Redshift" "Yaawn, it's almost $3time."
+		else
+			exec notify-send "Redshift" "Good $3. the sun is shining!"
+		fi
 esac
