99b74039ae23220a71d14f71a1de57f210efbe21
Francesco Minnocci <ad17fmin@uwcad.it>
2020-09-05 00:59:24 +0200

    Update picom.conf with sensible experimental defaults
    Former-commit-id: 1b8c4d35f42189998d3b10db70ad41f06190f527
    Former-commit-id: 695fa567ce5f6723dfc13a622cca0b2ee26e8266

--- .config/zsh/plugins/prompt/prompt.zsh
+++ .config/zsh/plugins/prompt/prompt.zsh
@@ -1,4 +1,4 @@
-## An alternative, lightweight prompt
+## Useful variables for prompt
 	local curdir="%~"
 	local hostname="%M"
 	local username="%n"
@@ -22,9 +22,9 @@
 	autoload -Uz prompt_special_chars
 	prompt_special_chars
 
-	PS1=$'%B%(!.%F{red}.%F{blue} %F{blue}$schars[333]$schars[262]$schars[261]$schars[260]%K{black}%F{yellow})%n%F{green}@%F{blue}%m$schars[260]$schars[261]$schars[262]$schars[333]%K{reset} %F{magenta}%~\n%F{cyan}$%F{reset} '
+	PS1=$'%B%(!.%F{red}.%F{blue} %F{blue}$schars[333]$schars[262]$schars[261]$schars[260]%K{black}%F{yellow})%n%F{green}@%F{blue}%m$schars[260]$schars[261]$schars[262]$schars[333]%K{reset} %F{magenta}%~\n%F{cyan}$%b%F{reset} '
 	# PS1="%B%F{blue}$mytime %F{green}[$username]%f %b$curdir $rootorwhat "
-	PS2='%B%F{yellow}•%b%F{reset} '
+	PS2='%B%F{magenta}•%b%F{reset} '
 	setopt transientrprompt	# self-explanatory
 	RPROMPT='${vim_mode}'
 	if [[ -n $SSH_CONNECTION ]]; then
