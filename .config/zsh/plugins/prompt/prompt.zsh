39607fd22aae8563f38af65e362c9cf1ee2d8609
Francesco Minnocci <ad17fmin@uwcad.it>
2020-09-23 22:01:51 +0200

    Add a nicer, nerdfontfree prompt for the tty.
    Former-commit-id: 5f7dbaf77980fe94e12f55d03d1ab9fafe44b499
    Former-commit-id: 0b6de0c428c735251cf225b52b3cb2e0f78b8e78

--- .config/zsh/plugins/prompt/prompt.zsh
+++ .config/zsh/plugins/prompt/prompt.zsh
@@ -21,9 +21,11 @@
 # Necessary for fadebar (stolen from zsh's bigfade prompt)
 	autoload -Uz prompt_special_chars
 	prompt_special_chars
-
-	PS1=$'%B%(!.%F{red}.%F{blue} %F{blue}$schars[333]$schars[262]$schars[261]$schars[260]%K{black}%F{yellow})%n%F{green}@%F{blue}%m$schars[260]$schars[261]$schars[262]$schars[333]%K{reset} %F{magenta}%~\n%F{cyan}$%b%F{reset} '
-	# PS1="%B%F{blue}$mytime %F{green}[$username]%f %b$curdir $rootorwhat "
+	if [ "$TERM" = "linux" ]; then
+    PS1="%B%F{blue}$mytime %F{green}[$username]%f %b$curdir $rootorwhat "
+  else
+    PS1=$'%B%(!.%F{red}.%F{blue} %F{blue}$schars[333]$schars[262]$schars[261]$schars[260]%K{black}%F{yellow})%n%F{green}@%F{blue}%m$schars[260]$schars[261]$schars[262]$schars[333]%K{reset} %F{magenta}%~\n%F{cyan}$%b%F{reset} '
+  fi
 	PS2='%B%F{magenta}•%b%F{reset} '
 	setopt transientrprompt	# self-explanatory
 	RPROMPT='${vim_mode}'
