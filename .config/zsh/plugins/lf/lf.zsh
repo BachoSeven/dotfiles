ee26775e8f0027f92602719bd463b6b2a56d013d
Francesco Minnocci <ad17fmin@uwcad.it>
2020-12-29 22:43:09 +0100

    Slight fix
    Former-commit-id: 9f3443156b24bcdf21300d49a75ad0fdadbbc8e9
    Former-commit-id: 77f5bab0dfa5f68b249707840b1ee34bd68b6837

--- .config/zsh/plugins/lf/lf.zsh
+++ .config/zsh/plugins/lf/lf.zsh
@@ -2,10 +2,10 @@ lfcd () {
     tmp="$(mktemp)"
     fid="$(mktemp)"
     lf -command '$printf $id > '"$fid"'' -last-dir-path="$tmp" "$@"
-    id="$(cat "$fid")"
+    id="$(\cat "$fid")"
     archivemount_dir="/tmp/__lf_archivemount_$id"
     if [ -f "$archivemount_dir" ]; then
-        cat "$archivemount_dir" | \
+        \cat "$archivemount_dir" | \
             while read -r line; do
                 sudo umount "$line"
                 rmdir "$line"
@@ -13,7 +13,7 @@ lfcd () {
         rm -f "$archivemount_dir"
     fi
     if [ -f "$tmp" ]; then
-        dir="$(cat "$tmp")"
+        dir="$(\cat "$tmp")"
         rm -f "$tmp" >/dev/null
         [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
     fi
