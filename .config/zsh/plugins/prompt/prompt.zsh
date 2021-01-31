511c7f9ccb36145cc15fb045366c94a8b8f64277
Francesco Minnocci <ad17fmin@uwcad.it>
2021-01-31 19:12:57 +0100

    Make coherent zsh and bash themes when in a tty

--- .config/zsh/plugins/prompt/prompt.zsh
+++ .config/zsh/plugins/prompt/prompt.zsh
@@ -22,7 +22,7 @@
 	autoload -Uz prompt_special_chars
 	prompt_special_chars
   if [ "$TERM" = "linux" ]; then # TTY prompt
-    PS1="%B%F{blue}$mytime %F{green}[$username]%f %b$curdir $rootorwhat "
+    PS1="%B$colorbg{blue}$mytime$uncolorbg %F{green}[$username]%f %b%F{magenta}$curdir%f $rootorwhat "
   else
     PS1=$'%B%F{blue} %F{blue}$schars[333]$schars[262]$schars[261]$schars[260]%(!.%F{red}.%K{black}%F{yellow})%n%F{green}@%F{blue}%m$schars[260]$schars[261]$schars[262]$schars[333]%k %F{magenta}%~\n%(?.%F{cyan}.%F{red}) %f'
   fi
