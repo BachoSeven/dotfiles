1373caf3e2cab8f469c0d8d26f66447268b1542a
Francesco Minnocci <ad17fmin@uwcad.it>
2021-03-02 15:09:05 +0100

    Add `vent` alias

--- .config/zsh/plugins/prompt/prompt.zsh
+++ .config/zsh/plugins/prompt/prompt.zsh
@@ -34,3 +34,15 @@
 	fi
 	RPS1+='${vim_mode}'
 	RPS2=$RPS1
+
+	# autoload -Uz vcs_info
+	# zstyle ':vcs_info:*' enable git svn
+	# precmd() {
+			# vcs_info
+	# }
+	# PS1+=' ${vcs_info_msg_0_}'
+	# zstyle ':vcs_info:*' actionformats \
+			# $'%F{4}\UE0A0%F{3} on %F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
+	# zstyle ':vcs_info:*' formats       \
+			# $'%F{4}\UE0A0%F{3} on %F{5}[%F{4}%b%F{5}]%f '
+	# zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
