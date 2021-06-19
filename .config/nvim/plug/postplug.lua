d053b15623a88eb75f9685634c2b929ac765ed16
Francesco Minnocci <ad17fmin@uwcad.it>
2021-06-20 01:20:20 +0200

    nvim-compe: enable snippets for certain filetypes; omni-comp x vimtex

--- .config/nvim/plug/postplug.lua
+++ .config/nvim/plug/postplug.lua
@@ -27,25 +27,31 @@ end
 
 -- Completion
 require'compe'.setup {
-	enabled = true;
-	autocomplete = true;
-	debug = false;
-	min_length = 1;
-	preselect = 'enable';
-	throttle_time = 80;
-	source_timeout = 200;
-	incomplete_delay = 400;
-	max_abbr_width = 100;
-	max_kind_width = 100;
-	max_menu_width = 100;
-	documentation = true;
+	enabled = true,
+	autocomplete = true,
+	debug = false,
+	min_length = 1,
+	preselect = 'enable',
+	throttle_time = 80,
+	source_timeout = 200,
+	incomplete_delay = 400,
+	max_abbr_width = 100,
+	max_kind_width = 100,
+	max_menu_width = 100,
+	documentation = true,
 	source = {
-		path = true;
-		buffer = true;
-		nvim_lsp = true;
-		ultisnips = true;
-	};
+		omni = {
+			filetypes = {'tex'},
+		},
+		ultisnips = {
+			filetypes = {'tex','c','cpp','markdown','text','sh','zsh','snippets','octave','conf'},
+		},
+		path = true,
+		buffer = true,
+		nvim_lsp = true,
+	},
 }
+
 -- Enable lsp snippets for nvim-compe
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
