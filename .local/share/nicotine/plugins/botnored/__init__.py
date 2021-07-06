c6f5f936ad876f7d92223bc0786c2fa5ab0ea077
Francesco Minnocci <ad17fmin@uwcad.it>
2021-07-06 17:47:48 +0200

    `sx` does not allow me to send both stdout and stderr to /dev/null

--- .local/share/nicotine/plugins/botnored/__init__.py
+++ .local/share/nicotine/plugins/botnored/__init__.py
@@ -3,7 +3,7 @@ from pynicotine.pluginsystem import BasePlugin
 class Plugin(BasePlugin):
     __name__ = "Botnored"
 
-    def UserResolveNotification(self, user, ip, port, country):  # noqa
+    def user_resolve_notification(self, user, ip, port, country):  # noqa
         if ip != '0.0.0.0' and port == 0:
             if not self.frame.np.network_filter.is_user_ignored(user):
                 self.frame.np.network_filter.ignore_user(user)
