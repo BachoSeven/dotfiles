60207cf8dc22ee1ef07e27bff5c0b9d2068e3f15
Francesco Minnocci <ad17fmin@uwcad.it>
2021-07-22 15:42:49 +0200

    xplr: Forgot to upload plug/init.lua

---
+++ .config/xplr/plug/init.lua
@@ -0,0 +1,39 @@
+local function setup()
+    package.path = os.getenv("XDG_CONFIG_HOME") .. '/xplr/plug/?/src/init.lua'
+
+    -- Add as submodules
+    require("completion").setup()
+    require("icons").setup()
+    require("material-landscape").setup()
+
+    require("dua-cli").setup{
+      mode = "action",
+      key = "D",
+    }
+    require("fzf").setup{
+      mode = "default",
+      key = "ctrl-f",
+      args = "--preview '${XDG_CONFIG_HOME:-$HOME/.config}/lf/scope {}'"
+    }
+    require("zoxide").setup{
+      mode = "default",
+      key = "Z"
+    }
+    require("dragon").setup{
+      mode = "selection_ops",
+      key = "D",
+      drag_args = "",
+      drop_args = "",
+      keep_selection = false
+    }
+
+    -- require("comex").setup()
+    -- require("xargs").setup{
+      -- key = "x",
+    -- }
+    -- require("paste-rs").setup{
+      -- db_path = "$XDG_DATA_HOME/paste.rs.list"
+    -- }
+end
+
+return { setup = setup }
