113f2b6de2bc851ba6aa985d3ca6c096c255fec9
Francesco Minnocci <ad17fmin@uwcad.it>
2023-08-05 19:53:16 +0200

    nvim: add julia lsp

--- .config/nvim/plug/postplug.lua
+++ .config/nvim/plug/postplug.lua
@@ -114,6 +114,11 @@ lsp.emmet_ls.setup {
 	on_attach = custom_lsp_attach,
 	capabilities = capabilities
 }
+lsp.julials.setup{
+	cmd={ "julia", "--startup-file=no", "--history-file=no", "-e", 'ls_install_path = joinpath(\n        get(DEPOT_PATH, 1, joinpath(homedir(), ".local")), "share", "julia", "environments", "v1.9"\n    )\n    pushfirst!(LOAD_PATH, ls_install_path)\n    using LanguageServer\n    popfirst!(LOAD_PATH)\n    depot_path = get(ENV, "JULIA_DEPOT_PATH", "")\n    project_path = let\n        dirname(something(\n            ## 1. Finds an explicitly set project (JULIA_PROJECT)\n            Base.load_path_expand((\n                p = get(ENV, "JULIA_PROJECT", nothing);\n                p === nothing ? nothing : isempty(p) ? nothing : p\n            )),\n            ## 2. Look for a Project.toml file in the current working directory,\n            ##    or parent directories, with $HOME as an upper boundary\n            Base.current_project(),\n            ## 3. First entry in the load path\n            get(Base.load_path(), 1, nothing),\n            ## 4. Fallback to default global environment,\n            ##    this is more or less unreachable\n            Base.load_path_expand("@v#.#"),\n        ))\n    end\n    @info "Running language server" VERSION pwd() project_path depot_path\n    server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)\n    server.runlinter = true\n    run(server)\n  ' },
+	on_attach = custom_lsp_attach,
+	capabilities = capabilities
+}
 lsp.texlab.setup {
 	settings = {
 		latex = {
@@ -149,7 +154,7 @@ vim.api.nvim_set_keymap("n", "<leader>a", ":ASToggle<CR>", {})
 
 -- Treesitter
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "c", "go", "vim", "latex", "toml", "bash", "html", "javascript", "json" },
+	ensure_installed = { "c", "go", "vim", "latex", "matlab", "markdown", "toml", "bash", "html", "javascript", "json", "julia" },
 	highlight = {
 		enable = true,
 		disable = { "latex" },
